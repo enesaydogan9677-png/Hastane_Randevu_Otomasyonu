5b8c016b8166e8b6fe532bb46e482c41045b7e0d
enesaydogan9677-png <enesaydogan9677@gmail.com>
2025-12-28 00:09:48 +0300

    Delete HastaneRandevuSistemi directory

--- HastaneRandevuSistemi/HastaneRandevuSistemi/Siniflar/FirebaseBilgiler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HastaneRandevuSistemi.Siniflar
-{
-    public static class FirebaseBilgiler
-    {
-        public const string FirebaseSecret = "kBBUiTpcSiWFEy9lmiERdYuphbfhxlbXzvkedjhB";
-
-        public const string FirebasePath = "https://ntb-projesi-default-rtdb.firebaseio.com/";
-    }
-}
